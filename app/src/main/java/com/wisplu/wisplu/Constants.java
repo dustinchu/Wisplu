16a0612f99c13e23d331c637eb923cb31186847b
wisplu <dustin_chu@hotmail.com>
2016-09-14 23:26:23 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/wisplu/wisplu/Constants.java
@@ -0,0 +1,11 @@
+package com.wisplu.wisplu;
+
+/**
+ * Created by shuikuhiro on 16/9/13.
+ */
+public class Constants {
+    public static final String APP_ID = "258f5624";
+    public static final String APP_KEY = "511a9fecc22d6397c740148b7e65ae5e";
+
+    public static final String BUCKET_NAME = "balance_book";
+}
